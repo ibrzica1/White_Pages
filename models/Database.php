8184a204db3af009eb739dd8ff64ad654b46051a
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-08-29 16:36:41 +0100

    Create Database and User classes

---
+++ models/Database.php
@@ -0,0 +1,11 @@
+<?php
+
+class Database
+{
+  public $connection;
+
+  public function __construct()
+  {
+    $this->connection = mysqli_connect("localhost","root","","white_pages");
+  }
+}
---
+++ models/User.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "Database.php";
+
+class User extends Database
+{
+  public $name;
+  public $email;
+  public $lozinka;
+}

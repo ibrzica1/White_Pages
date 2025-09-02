7ff95d2a58acd75a40a3f5bf037836246ff0466e
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-09-02 16:42:53 +0100

    Create business class

---
+++ businessInput.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>business Register</title>
+</head>
+
+<body>
+  
+    <form action="models/registerBusiness.php" method="post">
+        <input type="text" name="business_id" placeholder="Business Id">
+        <input type="text" name="address" placeholder="Address">
+        <input type="text" name="foundet" placeholder="Founded">
+        <input type="text" name="employees" placeholder="Employees">
+        <input type="text" name="revenue" placeholder="Revenue">
+        <button>Register</button>
+    </form>
+
+</body>
+
+</html>
---
+++ models/Business.php
@@ -0,0 +1,11 @@
+<?php
+
+class Business extends Database
+{
+  public $business_id;
+  public $user_id;
+  public $address;
+  public $founded;
+  public $employees;
+  public $revenue;
+}

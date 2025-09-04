0b59ad744faa5d693503dff4cfcf32fd14708169
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-09-04 17:33:44 +0100

    Complete Search by Business Address

---
+++ searchBusinAddress.php
@@ -0,0 +1,73 @@
+<?php
+
+if(session_status() == PHP_SESSION_NONE)
+  {
+    session_start();
+  }
+
+  if(!isset($_GET["inputBusinAddress"]) || empty($_GET["inputBusinAddress"]))
+{
+  $_SESSION["message"] = "You didnt send input Business Address";
+  header("Location: index.php");
+  exit();
+}
+else
+{
+  $inputBusinAddress = $_GET["inputBusinAddress"];
+}
+
+require_once "models/Database.php";
+
+$database = new Database();
+
+$inputBusinAddress = $database->connection->real_escape_string($inputBusinAddress);
+
+$result = $database->connection->query("SELECT * FROM business WHERE address LIKE '%$inputBusinAddress%' ");
+
+$businessArray = $result->fetch_all(MYSQLI_ASSOC);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Search By Business Address</title>
+
+</head>
+
+<body>
+
+  <?php if(isset($_SESSION["logged"])): ?>
+    <a href="index.php">Main Page</a>
+    <a href="businessInput.php">Register Business</a>
+    <p><?= $_SESSION["username"] ?></p>
+    <a href="logout.php">Logout</a>
+    <?php else: ?>
+      <a href="index.php">Main Page</a>
+      <a href="login.php">Log In</a>
+      <a href="signup.php">Sign Up</a>
+  <?php endif; ?>
+
+ <?php if(empty($businessArray)):?>
+  <h3>Search By Business Address</h3>
+    <h4>Results for %<?=$inputBusinAddress?>%: <span>No Results</span></h4>
+ <?php else:?>
+  <h3>Search By Business Address</h3>
+    <h4>Results for %<?=$inputBusinAddress?>%:</h4>
+    <?php foreach($businessArray as $business):?>
+      <p>Business ID: <?=$business['business_id']?></p>
+      <p>Name: <?=$business['name']?></p>
+      <p>Address: <?=$business['address']?></p>
+      <p>Founded: <?=$business['founded']?></p>
+      <p>Employees: <?=$business['employees']?></p>
+      <p>Revenue: <?=$business['revenue']?></p>
+    <?php endforeach;?>
+ <?php endif;?>
+
+</body>
+
+
+</html>

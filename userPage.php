58b600e33e99cbba3190d5d240b052830ae413f8
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-09-14 05:48:40 +0100

    Create CSS for User Page

--- userPage.php
+++ userPage.php
@@ -27,6 +27,10 @@ $businessCount = $user->getNumberUserBusiness($userId);
   
   <title>User Page</title>
  <link rel="stylesheet" href="style/header.css"> 
+ <link rel="stylesheet" href="style/userPage.css"> 
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -49,73 +53,110 @@ $businessCount = $user->getNumberUserBusiness($userId);
     </div>
 </div>
 
-<h2><?=$sessionUser["username"]?></h2>
+<div class="user-body">
 
-<?php if(isset($_SESSION["message"])): ?>
-    <?php $message = htmlspecialchars($_SESSION["message"]); ?>
-    <p><?=  $message ?></p>
-    <?php unset($_SESSION["message"]); ?>
-<?php endif; ?>
+  <div class="user-info-container">
 
-<div class="username-container">
-  <div class="username-display">
-    <h3><?=$sessionUser["username"]?></h3>
-    <button onclick="showEditForm('username')">Edit</button>
+      <h2><?=$sessionUser["username"]?></h2>
+
+      <?php if(isset($_SESSION["message"])): ?>
+          <?php $message = htmlspecialchars($_SESSION["message"]); ?>
+          <div class="userPage-message">
+            <p><?=  $message ?></p>
+          </div>
+          <?php unset($_SESSION["message"]); ?>
+      <?php endif; ?>
+
+    <div class="username-container">
+      <p>Username</p>
+      <div class="username-display">
+        <img src="images\user (2).png">
+        <h3><?=$sessionUser["username"]?></h3>
+        <button onclick="showEditForm('username')">Edit</button>
+      </div>
+
+      <div class="username-edit-form" style="display: none;">
+        <form action="models/updateUser.php" method="post" class="username-form">
+          <img src="images\user (2).png">
+          <input type="hidden" name="field" value="username">
+          <input type="text" name="newUsername" placeholder="<?=$sessionUser["username"]?>">
+          <button type="submit" class="save-btn">Save</button>
+          <button type="button" onclick="hideEditForm('username')" class="cancel-btn">Cancel</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="email-container">
+      <p>Email</p>
+      <div class="email-display">
+        <img src="images\mail.png">
+        <h3><?=$sessionUser["email"]?></h3>
+        <button onclick="showEditForm('email')">Edit</button>
+      </div>
+
+      <div class="email-edit-form" style="display: none;">
+        <form action="models/updateUser.php" method="post" class="email-form">
+          <img src="images\mail.png">
+          <input type="hidden" name="field" value="email">
+          <input type="text" name="newEmail" placeholder="<?=$sessionUser["email"]?>">
+          <button type="submit" class="save-btn">Save</button>
+          <button type="button" onclick="hideEditForm('email')" class="cancel-btn">Cancel</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="password-container">
+      <p>Password</p>
+      <div class="password-display">
+        <img src="images\password.png">
+        <h3>*********</h3>
+        <button onclick="showEditForm('password')">Edit</button>
+      </div>
+
+      <div class="password-edit-form" style="display: none;">
+        <form action="models/updateUser.php" method="post"  class="password-form">
+            
+            <input type="hidden" name="field" value="password">
+          <div class="password-input-container">
+            <img src="images\old-key.png">
+            <input type="text" name="oldPassword" placeholder="Old Password">
+          </div>
+          <div class="password-input-container">
+            <img src="images\password.png">
+            <input type="text" name="newPassword" placeholder="New Password">
+          </div>
+          <div class="password-input-container">
+            <img src="images\security.png">
+            <input type="text" name="repeatPassword" placeholder="Repeat Password">
+          </div>
+          <div class="password-btn-container">
+          <button type="submit" class="save-btn">Save</button>
+          <button type="button" onclick="hideEditForm('password')" class="cancel-btn">Cancel</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    
+    <div class="info-container">
+      <p>Business Registered</p>
+      <div class="info-display">
+        <img src="images\briefcase.png">
+        <h3>Business Registered <?=$businessCount?></h3>
+        <a href="userBusiness.php">Edit</a>
+      </div>
+    </div>
+    
+    <div class="delete-btn-container">
+      <form action="models/deleteUser.php" method="post" onsubmit="return confirm('Are you sure?');">
+        <button>Delete account</button>
+      </form>
+    </div>
+
   </div>
 
-  <div class="username-edit-form" style="display: none;">
-    <form action="models/updateUser.php" method="post">
-      <input type="hidden" name="field" value="username">
-      <input type="text" name="newUsername" placeholder="<?=$sessionUser["username"]?>">
-      <button type="submit">Save</button>
-      <button type="button" onclick="hideEditForm('username')">Cancel</button>
-    </form>
-  </div>
 </div>
 
-<div class="email-container">
-  <div class="email-display">
-    <h3><?=$sessionUser["email"]?></h3>
-    <button onclick="showEditForm('email')">Edit</button>
-  </div>
 
-  <div class="email-edit-form" style="display: none;">
-    <form action="models/updateUser.php" method="post">
-      <input type="hidden" name="field" value="email">
-      <input type="text" name="newEmail" placeholder="<?=$sessionUser["email"]?>">
-      <button type="submit">Save</button>
-      <button type="button" onclick="hideEditForm('email')">Cancel</button>
-    </form>
-  </div>
-</div>
-
-<div class="password-container">
-  <div class="password-display">
-    <h3>*********</h3>
-    <button onclick="showEditForm('password')">Edit</button>
-  </div>
-
-  <div class="password-edit-form" style="display: none;">
-    <form action="models/updateUser.php" method="post">
-      <input type="hidden" name="field" value="password">
-      <input type="text" name="oldPassword" placeholder="Old Password">
-      <input type="text" name="newPassword" placeholder="New Password">
-      <input type="text" name="repeatPassword" placeholder="Repeat Password">
-      <button type="submit">Save</button>
-      <button type="button" onclick="hideEditForm('username')">Cancel</button>
-    </form>
-  </div>
-</div>
-
-<div>
-  <h3>Business Registered <?=$businessCount?></h3>
-  <a href="userBusiness.php">Edit</a>
-</div>
-<div>
-  <form action="models/deleteUser.php" method="post" onsubmit="return confirm('Are you sure?');">
-    <button>Delete account</button>
-  </form>
-</div>
 
 
 <script>
@@ -127,7 +168,7 @@ $businessCount = $user->getNumberUserBusiness($userId);
 
   function hideEditForm(field)
   {
-    document.querySelector(`.${field}-display`).style.display = 'block';
+    document.querySelector(`.${field}-display`).style.display = 'flex';
     document.querySelector(`.${field}-edit-form`).style.display = 'none';
   }
 </script>

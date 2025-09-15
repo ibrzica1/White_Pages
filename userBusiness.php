8cc0a7c19865877459e50edf2b82e86ba9bf33b7
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-09-15 08:21:15 +0100

    Create CSS for User Business

--- userBusiness.php
+++ userBusiness.php
@@ -28,6 +28,10 @@ $userBusiness = $business->getBusinessByUserId($userId);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>User Business</title>
   <link rel="stylesheet" href="style/header.css">
+  <link rel="stylesheet" href="style/userBusiness.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -49,143 +53,171 @@ $userBusiness = $business->getBusinessByUserId($userId);
      </div>
     </div>
   </div>
+
+  <div class="user-business-body">
+
+    <?php if(isset($_SESSION["message"])): ?>
+        <?php $message = htmlspecialchars($_SESSION["message"]); ?>
+        <p><?=  $message ?></p>
+        <?php unset($_SESSION["message"]); ?>
+    <?php endif; ?>
+
+    <?php if(empty($userBusiness)): ?>
+      <?php 
+      $_SESSION["message"] = "There is no registered business";
+      header("Location: userPage.php");
+      exit(); 
+      ?>
+    <?php else: ?>
+      <?php foreach($userBusiness as $business): ?>
+
+        <div class="user-business">
+
+        <div class="business_id-container">
+          <h3>Business ID</h3>
+          <div class="business_id-display">
+            <img src="images\fingerprint.png">
+            <p><?=$business['business_id']?></p>
+            <button onclick="showEditForm('business_id')" class="edit-btn">Edit</button>
+          </div>
+
+          <div class="business_id-edit-form" style="display: none;">
+            <form action="models/updateBusiness.php" method="post" class="business_id-form">
+              <input type="hidden" name="field" value="business_id">
+              <input type="hidden" name="id" value="<?=$business["id"]?>">
+              <img src="images\fingerprint.png">
+              <input type="text" name="newBusiness_id" placeholder="<?=$business['business_id']?>">
+              <button type="submit"  class="save-btn">Save</button>
+              <button type="button" onclick="hideEditForm('business_id')" class="cancel-btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="name-container">
+          <h3>Name</h3>
+          <div class="name-display">
+            <img src="images\briefcase.png">
+            <p><?=$business['name']?></p>
+            <button onclick="showEditForm('name')" class="edit-btn">Edit</button>
+          </div>
+
+          <div class="name-edit-form" style="display: none;">
+            <form action="models/updateBusiness.php" method="post" class="name-form">
+              <input type="hidden" name="field" value="name">
+              <input type="hidden" name="id" value="<?=$business["id"]?>">
+              <img src="images\briefcase.png">
+              <input type="text" name="newName" placeholder="<?=$business['name']?>">
+              <button type="submit" class="save-btn">Save</button>
+              <button type="button" onclick="hideEditForm('name')" class="cancel-btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="address-container">
+          <h3>Address</h3>
+          <div class="address-display">
+            <img src="images\location.png">
+            <p><?=$business['address']?></p>
+            <button onclick="showEditForm('address')" class="edit-btn">Edit</button>
+          </div>
+
+          <div class="address-edit-form" style="display: none;">
+            <form action="models/updateBusiness.php" method="post" class="address-form">
+              <input type="hidden" name="field" value="address">
+              <input type="hidden" name="id" value="<?=$business["id"]?>">
+              <img src="images\location.png">
+              <input type="text" name="newAddress" placeholder="<?=$business['address']?>">
+              <button type="submit" class="save-btn">Save</button>
+              <button type="button" onclick="hideEditForm('address')" class="cancel-btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="founded-container">
+          <h3>Founded</h3>
+          <div class="founded-display">
+            <img src="images\pantheon.png">
+            <p><?=$business['founded']?></p>
+            <button onclick="showEditForm('founded')" class="edit-btn">Edit</button>
+          </div>
+
+          <div class="founded-edit-form" style="display: none;">
+            <form action="models/updateBusiness.php" method="post" class="founded-form">
+              <input type="hidden" name="field" value="founded">
+              <input type="hidden" name="id" value="<?=$business["id"]?>">
+              <img src="images\pantheon.png">
+              <input type="date" name="newFounded" placeholder="<?=$business['founded']?>">
+              <button type="submit" class="save-btn">Save</button>
+              <button type="button" onclick="hideEditForm('founded')" class="cancel-btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="employees-container">
+          <h3>Employees</h3>
+          <div class="employees-display">
+            <img src="images\employee.png">
+            <p><?=$business['employees']?></p>
+            <button onclick="showEditForm('employees')" class="edit-btn">Edit</button>
+          </div>
+
+          <div class="employees-edit-form" style="display: none;">
+            <form action="models/updateBusiness.php" method="post" class="employees-form">
+              <input type="hidden" name="field" value="employees">
+              <input type="hidden" name="id" value="<?=$business["id"]?>">
+              <img src="images\employee.png">
+              <input type="text" name="newEmployees" placeholder="<?=$business['employees']?>">
+              <button type="submit" class="save-btn">Save</button>
+              <button type="button" onclick="hideEditForm('employees')" class="cancel-btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="revenue-container">
+          <h3>Revenue</h3>
+          <div class="revenue-display">
+            <img src="images\salary.png">
+            <p><?=$business['revenue']?></p>
+            <button onclick="showEditForm('revenue')" class="edit-btn">Edit</button>
+          </div>
+
+          <div class="revenue-edit-form" style="display: none;">
+            <form action="models/updateBusiness.php" method="post" class="revenue-form">
+              <input type="hidden" name="field" value="revenue">
+              <input type="hidden" name="id" value="<?=$business["id"]?>">
+              <img src="images\salary.png">
+              <input type="text" name="newRevenue" placeholder="<?=$business['revenue']?>">
+              <button type="submit" class="save-btn">Save</button>
+              <button type="button" onclick="hideEditForm('revenue')" class="cancel-btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="delete-btn-container">
+          <form action="models/deleteBusiness.php" method="post" onsubmit="return confirm('Are you sure you want do delete <?=$business['name']?>')">
+            <input type="hidden" name="delete" value="<?=$business["id"]?>">
+            <button>Delete Business</button>
+          </form>
+        </div>
+
+        </div>
+      <?php endforeach; ?>
+    <?php endif; ?>
+
+  </div>
   
-<?php if(isset($_SESSION["message"])): ?>
-    <?php $message = htmlspecialchars($_SESSION["message"]); ?>
-    <p><?=  $message ?></p>
-    <?php unset($_SESSION["message"]); ?>
-<?php endif; ?>
 
-<?php if(empty($userBusiness)): ?>
-  <?php 
-   $_SESSION["message"] = "There is no registered business";
-   header("Location: userPage.php");
-   exit(); 
-  ?>
-<?php else: ?>
-  <?php foreach($userBusiness as $business): ?>
-    <div class="business_id-container">
-      <div class="business_id-display">
-        <h3>Business ID: <span><?=$business['business_id']?></span></h3>
-        <button onclick="showEditForm('business_id')">Edit</button>
-      </div>
-
-      <div class="business_id-edit-form" style="display: none;">
-        <form action="models/updateBusiness.php" method="post">
-          <input type="hidden" name="field" value="business_id">
-          <input type="hidden" name="id" value="<?=$business["id"]?>">
-          <input type="text" name="newBusiness_id" placeholder="<?=$business['business_id']?>">
-          <button type="submit">Save</button>
-          <button type="button" onclick="hideEditForm('business_id')">Cancel</button>
-        </form>
-      </div>
-    </div>
-
-    <div class="name-container">
-      <div class="name-display">
-        <h3>Name: <span><?=$business['name']?></span></h3>
-        <button onclick="showEditForm('name')">Edit</button>
-      </div>
-
-      <div class="name-edit-form" style="display: none;">
-        <form action="models/updateBusiness.php" method="post">
-          <input type="hidden" name="field" value="name">
-          <input type="hidden" name="id" value="<?=$business["id"]?>">
-          <input type="text" name="newName" placeholder="<?=$business['name']?>">
-          <button type="submit">Save</button>
-          <button type="button" onclick="hideEditForm('name')">Cancel</button>
-        </form>
-      </div>
-    </div>
-
-    <div class="address-container">
-      <div class="address-display">
-        <h3>Address: <span><?=$business['address']?></span></h3>
-        <button onclick="showEditForm('address')">Edit</button>
-      </div>
-
-      <div class="address-edit-form" style="display: none;">
-        <form action="models/updateBusiness.php" method="post">
-          <input type="hidden" name="field" value="address">
-          <input type="hidden" name="id" value="<?=$business["id"]?>">
-          <input type="text" name="newAddress" placeholder="<?=$business['address']?>">
-          <button type="submit">Save</button>
-          <button type="button" onclick="hideEditForm('address')">Cancel</button>
-        </form>
-      </div>
-    </div>
-
-    <div class="founded-container">
-      <div class="founded-display">
-        <h3>Founded: <span><?=$business['founded']?></span></h3>
-        <button onclick="showEditForm('founded')">Edit</button>
-      </div>
-
-      <div class="founded-edit-form" style="display: none;">
-        <form action="models/updateBusiness.php" method="post">
-          <input type="hidden" name="field" value="founded">
-          <input type="hidden" name="id" value="<?=$business["id"]?>">
-          <input type="date" name="newFounded" placeholder="<?=$business['founded']?>">
-          <button type="submit">Save</button>
-          <button type="button" onclick="hideEditForm('founded')">Cancel</button>
-        </form>
-      </div>
-    </div>
-
-    <div class="employees-container">
-      <div class="employees-display">
-        <h3>Employees: <span><?=$business['employees']?></span></h3>
-        <button onclick="showEditForm('employees')">Edit</button>
-      </div>
-
-      <div class="employees-edit-form" style="display: none;">
-        <form action="models/updateBusiness.php" method="post">
-          <input type="hidden" name="field" value="employees">
-          <input type="hidden" name="id" value="<?=$business["id"]?>">
-          <input type="text" name="newEmployees" placeholder="<?=$business['employees']?>">
-          <button type="submit">Save</button>
-          <button type="button" onclick="hideEditForm('employees')">Cancel</button>
-        </form>
-      </div>
-    </div>
-
-    <div class="revenue-container">
-      <div class="revenue-display">
-        <h3>Revenue: <span><?=$business['revenue']?></span></h3>
-        <button onclick="showEditForm('revenue')">Edit</button>
-      </div>
-
-      <div class="revenue-edit-form" style="display: none;">
-        <form action="models/updateBusiness.php" method="post">
-          <input type="hidden" name="field" value="revenue">
-          <input type="hidden" name="id" value="<?=$business["id"]?>">
-          <input type="text" name="newRevenue" placeholder="<?=$business['revenue']?>">
-          <button type="submit">Save</button>
-          <button type="button" onclick="hideEditForm('revenue')">Cancel</button>
-        </form>
-      </div>
-    </div>
-
-    <div>
-      <form action="models/deleteBusiness.php" method="post" onsubmit="return confirm('Are you sure you want do delete <?=$business['name']?>')">
-        <input type="hidden" name="delete" value="<?=$business["id"]?>">
-        <button>Delete</button>
-      </form>
-      
-    </div>
-  <?php endforeach; ?>
-<?php endif; ?>
 
 <script>
   function showEditForm(field)
   {
     document.querySelector(`.${field}-display`).style.display = 'none';
-    document.querySelector(`.${field}-edit-form`).style.display = 'block';
+    document.querySelector(`.${field}-edit-form`).style.display = 'flex';
   }
 
   function hideEditForm(field)
   {
-    document.querySelector(`.${field}-display`).style.display = 'block';
+    document.querySelector(`.${field}-display`).style.display = 'flex';
     document.querySelector(`.${field}-edit-form`).style.display = 'none';
   }
 </script>
